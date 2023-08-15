c5a97056aa86107b1b327deb6266a1dd3365bb63
Jobe Moraes <jobecamera@gmail.com>
2023-08-15 10:56:39 -0300

    hash.cpp com colisoes

--- Semana 04/Tabela Hash - Com colisoes/hash.cpp
+++ Semana 04/Tabela Hash - Com colisoes/hash.cpp
@@ -32,14 +32,50 @@ int Hash::obterTamanhoAtual()
 
 void Hash::inserir(Aluno aluno)
 {
-  int local = FuncaoHash(aluno);
-  estrutura[local] = aluno;
-  quant_itens++;
+  if (estaCheio())
+  {
+    cout << "A tabela Hash esta cheia!\n";
+    cout << "O aluno nao pode ser inserido.\n";
+  }
+  else
+  {
+    int local = FuncaoHash(aluno);
+
+    // se for > 0 signifca que já adicionei nesse local, se for < 0 (-1 ou -2) está vazio ou disponível, então posso inserir
+    while (estrutura[local].obterRa() > 0)
+    {
+      local = (local + 1) % max_posicoes;
+    }
+
+    estrutura[local] = aluno;
+    quant_itens++;
+  }
 }
 
 void Hash::deletar(Aluno aluno)
 {
   int local = FuncaoHash(aluno);
+  bool encontrado = false;
+
+  // procura posicões que não são -1 (vazio)
+  while (estrutura[local].obterRa() != -1)
+  {
+    if (estrutura[local].obterRa() == aluno.obterRa())
+    {
+      cout << "Aluno removido!\n";
+      estrutura[local] = Aluno(-2, " "); // (-2) = removido = disponível
+      quant_itens--;
+      encontrado = true;
+      break;
+    }
+    local = (local + 1) % max_posicoes;
+  }
+
+  if (!encontrado)
+  {
+    cout << "Aluno nao encontrado!\n";
+    cout << "Nenhum aluno foi removido.\n";
+  }
 
   if (estrutura[local].obterRa() != -1)
   {
@@ -51,16 +87,18 @@ void Hash::deletar(Aluno aluno)
 void Hash::buscar(Aluno &aluno, bool &busca)
 {
   int local = FuncaoHash(aluno);
-  Aluno aux = estrutura[local];
+  busca = false;
 
-  if (aluno.obterRa() != aux.obterRa())
+  // procura posicões que não são -1 (vazio)
+  while (estrutura[local].obterRa() != -1)
   {
-    busca = false;
-  }
-  else
-  {
-    busca = true;
-    aluno = aux;
+    if (estrutura[local].obterRa() == aluno.obterRa())
+    {
+      busca = true;
+      aluno = estrutura[local];
+      break;
+    }
+    local = (local + 1) % max_posicoes;
   }
 }
 
@@ -70,7 +108,8 @@ void Hash::imprimir()
 
   for (int i = 0; i < max_posicoes; i++)
   {
-    if (estrutura[i].obterRa() != -1)
+    // só imprime os itens que existem. Disponíveis e vazios são ignorados (-2 e -1)
+    if (estrutura[i].obterRa() > 0)
     {
       cout << "Indice " << i << ":" << estrutura[i].obterRa() << " " << estrutura[i].obterNome() << endl;
     }
