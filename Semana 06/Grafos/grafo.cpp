ef5a581b8daf2326d4d6beb499ba89601b47df33
Jobe Moraes <jobecamera@gmail.com>
2023-08-30 11:56:39 -0300

    definindo o metodo construtor

---
+++ Semana 06/Grafos/grafo.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include "grafo.h"
+using namespace std;
+
+Grafo::Grafo(int max, int valorArestaNula)  // construtor
+{
+  arestaNula = 0;
+  maxVertices = max;
+  numVertices = valorArestaNula;
+
+  vertices = new TipoItem[maxVertices];
+
+  matrizAdjacencias = new int *[maxVertices]; // vetor de vetores de inteiros
+  for (int i = 0; i < maxVertices; i++)
+  {
+    matrizAdjacencias[i] = new int[maxVertices];
+  }
+
+  for (int i = 0; i < maxVertices; i++) // linha
+  {
+    for (int j = 0; j < maxVertices; j++) // coluna
+    {
+      matrizAdjacencias[i][j] = arestaNula;
+    }
+  }
+}
+
+Grafo::~Grafo() {}
+
+int Grafo::obterIndice(TipoItem item) {}
+
+bool Grafo::estaCheio() {}
+
+void Grafo::insereVertice(TipoItem item) {}
+
+void Grafo::insereAresta(TipoItem noSaida, TipoItem noEntrada, int peso) {}
+
+int Grafo::obterPeso(TipoItem noSaida, TipoItem noEntrada) {}
+
+int Grafo::obterGrau(TipoItem item) {}
+
+void Grafo::imprimirMatriz() {}
+
+void Grafo::imprimirVertice() {}
